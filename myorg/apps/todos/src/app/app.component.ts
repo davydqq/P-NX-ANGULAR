82d67f3c774570455bea58d5534ec8737afddf6a
Davyd Konopatskyi <davydqq@gmail.com>
2021-11-19 20:30:46 +0200

    add lib

--- myorg/apps/todos/src/app/app.component.ts
+++ myorg/apps/todos/src/app/app.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Todo } from '@myorg/data';
+
 
 @Component({
   selector: 'myorg-root',
@@ -6,5 +9,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'todos';
+  todos: Todo[] = [];
+
+  constructor(private http: HttpClient) {
+    this.fetch();
+  }
+
+  fetch() {
+    this.http.get<Todo[]>('/api/todos').subscribe((t) => (this.todos = t));
+  }
+
+  addTodo() {
+    this.http.post('/api/addTodo', {}).subscribe(() => {
+      this.fetch();
+    });
+  }
 }
